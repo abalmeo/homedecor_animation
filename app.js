4c5b55fe0841046ccfd83da945a659ec9302a1fe
Alvin Balmeo <albalmeo@gmail.com>
2019-10-06 14:39:42 -0700

    Finishing up animation for all three sections

--- app.js
+++ app.js
@@ -11,15 +11,29 @@ new fullpage('#homedecor', {
     // Animating title with duration from one position to the next
     tl.fromTo(title, 0.5, { y: '50', opacity: 0 }, { y: 0, opacity: 1 });
 
+    // Section two animations
     if (destination.index === 1) {
       const chairs = document.querySelectorAll('.chair');
       const description = document.querySelector('.description');
 
+      // Animating chair and text sliding in
       tl.fromTo(chairs, 0.7, { x: '100%' }, { x: '-35%' })
         .fromTo(description, 0.5, { y: '50', opacity: 0 }, { y: 0, opacity: 1 })
         .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
         .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
         .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+    }
+
+    // Animate section 2
+    if (destination.index === 2) {
+      const descriptionTwo = document.querySelector('.description-two');
+
+      tl.fromTo(
+        descriptionTwo,
+        0.5,
+        { y: '-50', opacity: 0 },
+        { y: 0, opacity: 1 }
+      );
     }
   }
 });
